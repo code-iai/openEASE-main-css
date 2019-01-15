5c8e8e19f5a8aee86fe6abd09cb37b9fbe17d740
Rollson Ipo <rollsonipo@gmail.com>
2019-01-15 15:03:08 +0100

    test(build.test.js): adds dummy test to create a template for the build test

---
+++ test/build.test.js
@@ -0,0 +1,7 @@
+test('Should build valid layout.css', () => {
+    //Read layout_original.css as string
+    //Read layout.css as string
+    //cmp those to strings
+    //expect(layout_original_text).toBe(layout_text);
+    expect(true).toBe(true);
+});
