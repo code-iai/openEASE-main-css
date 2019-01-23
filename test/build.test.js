760548ebd39ba5bcc580ff34a41585c39bbef70f
Rollson Ipo <rollsonipo@gmail.com>
2019-01-23 16:25:43 +0100

    refactor(_user.scss): refactor user.scss
    refactor user.scss into adt-designator.scss  menu-bar.scss navbar.scss replay-page.scss
    teaching.scss

--- test/build.test.js
+++ test/build.test.js
@@ -1,7 +1,28 @@
-test('Should build valid layout.css', () => {
-    //Read layout_original.css as string
-    //Read layout.css as string
-    //cmp those to strings
-    //expect(layout_original_text).toBe(layout_text);
-    expect(true).toBe(true);
+const fs = require('fs');
+const path = require('path');
+const del = require('../node_modules/del');
+const webBuildTool = require('@code-iai/web-build-tool');
+
+const TEMP = path.join('test', 'temp');
+const FILEPATH_ORIGINAL = path.join('layout-original-scss', 'layout_original.scss');
+const RESULT_FILEPATH_ORIGINAL = path.join(TEMP, 'layout_original.css');
+const FILEPATH_NEW = 'layout.scss';
+const RESULT_FILEPATH_NEW = path.join(TEMP, 'layout.css');
+
+beforeEach(async () => {
+    fs.mkdirSync(TEMP);
+    await webBuildTool.scss.build({sourceFilePath: FILEPATH_ORIGINAL, resultFilePath: RESULT_FILEPATH_ORIGINAL, minify:true});
+    await webBuildTool.scss.build({sourceFilePath: FILEPATH_NEW, resultFilePath: RESULT_FILEPATH_NEW, minify:true});
 });
+
+afterEach(() => {
+    del.sync([TEMP], {force: true});
+});
+
+test('Should build valid layout.css', () => {
+    const layout_original_text = fs.readFileSync(RESULT_FILEPATH_ORIGINAL, 'utf8');
+    const layout_text = fs.readFileSync(RESULT_FILEPATH_NEW, 'utf8');
+    expect(layout_original_text).toEqual(layout_text);
+});
+
+
